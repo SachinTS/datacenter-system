f05f86b6f4e628674ce890e71df179c734322e2b
RobStanton1 <55086671+RobStanton1@users.noreply.github.com>
2019-11-23 19:13:12 +0000

    Add files via upload
    slight modification to the RM algorithm.
    
    No significant changes

--- resourceManagerAlgorithm.py
+++ resourceManagerAlgorithm.py
@@ -1,12 +1,12 @@
 import random
+import unittest
 
 # assumptions
 # jobs of type [A,B,C,D]
 # servers are stored as dictionaries
 
 # RAM and CPU capacity of servers
-RAMLimit = 64
-CPULimit = 18
+
 
 # Array of servers keyed by address servers[0][RAM] returns
 # the current RAM load of server 0 etc.
@@ -15,6 +15,7 @@ CPULimit = 18
 
 #   RAM             Cores       Server
 #   Allocated       Allocated   Status (Active/Idle)
+'''
 servers = {   
     0:{"RAM":62,    "Cores":15, "Status":1},
     1:{"RAM":60,    "Cores":12, "Status":1},
@@ -27,29 +28,28 @@ servers = {
     8:{"RAM":0,     "Cores":0,  "Status":0},
     9:{"RAM":0,     "Cores":0,  "Status":0}
 }
-
-jobTypes = {
-    "A":{"RAM":4,   "Cores":1},
-    "B":{"RAM":4,   "Cores":2},
-    "C":{"RAM":16,  "Cores":2},
-    "D":{"RAM":32,  "Cores":3},
-    "E":{"RAM":64,  "Cores":18}
-}
-
-job1 = "A"
-job2 = "B"
-job3 = "C"
-job4 = "D"
+'''
 
 
-
-def findOptimalServer(job, servers, types):
+def findOptimalServer(job, servers):
     # the function will take a local search like approach
     # feasible, already powered, server addresses will be added to an array
     # the server chosen should be the one with the lowest evaluation
     # if no already powered server is feasible then one will be powered
     # at random and the job will be allocated
-    
+
+    # some information known prior
+
+    RAMLimit = 64
+    CPULimit = 18
+
+    types = {
+        "A":{"RAM":4,   "Cores":1},
+        "B":{"RAM":4,   "Cores":2},
+        "C":{"RAM":16,  "Cores":2},
+        "D":{"RAM":32,  "Cores":3}
+    }
+
     # parameters:
     # job is a one letter code referring to the type of container being requested
     # servers is an array containing the current statuses of the servers
@@ -97,16 +97,85 @@ def findOptimalServer(job, servers, types):
         if idleServers:
             optimalServer = random.choice([s for s in servers if servers[s]["Status"] == 0])
         else:
-            return(False)
-
-    print(activeServers)
-    print(feasibleServers)
-    print(optimalServer)
+            return(None)
 
     return(optimalServer)
 
 
-findOptimalServer("E", servers, jobTypes)
+class resourceManagerAlgorithmTests(unittest.TestCase):
+
+    RAMLimit = 64
+    CPULimit = 18
+
+
+    # no active servers, no feasible servers, no idle servers
+    caseOneServers = {
+
+    }
+
+    # no active servers, no feasible servers, idle servers
+    caseTwoServers = {
+        0:{"RAM":0,    "Cores":0, "Status":0},
+        1:{"RAM":0,    "Cores":0, "Status":0},
+        2:{"RAM":0,    "Cores":0,  "Status":0},
+        3:{"RAM":0,    "Cores":0,  "Status":0}
+    }
+    
+    # active servers, no feasible servers, idle servers
+    caseThreeServers = {
+        0:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        1:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        2:{"RAM":RAMLimit,    "Cores":CPULimit,  "Status":1},
+        3:{"RAM":RAMLimit,    "Cores":CPULimit,  "Status":1}
+    }
+
+    # active servers, no feasible servers, idle servers
+    caseFourServers = {
+        0:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        1:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        2:{"RAM":0,    "Cores":0,  "Status":0},
+        3:{"RAM":0,    "Cores":0,  "Status":0}
+    }
+
+    # active servers, feasible servers, no idle servers
+    caseFiveServers = {
+        0:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        1:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        2:{"RAM":RAMLimit,    "Cores":CPULimit,  "Status":1},
+        3:{"RAM":16,    "Cores":6,  "Status":1},
+        4:{"RAM":4,    "Cores":1,  "Status":1}
+    }
+
+    # active servers,  feasible servers, idle servers
+    caseSixServers = {
+        0:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        1:{"RAM":RAMLimit,    "Cores":CPULimit, "Status":1},
+        2:{"RAM":16,    "Cores":6,  "Status":1},
+        3:{"RAM":4,    "Cores":1,  "Status":1},
+        4:{"RAM":0,    "Cores":0,  "Status":0},
+        5:{"RAM":0,    "Cores":0,  "Status":0}
+    }
+
+    def testCaseOne(self):
+        self.assertEqual(findOptimalServer("A", self.caseOneServers), None)
+
+    def testCaseTwo(self):
+        self.assertIn(findOptimalServer("A", self.caseTwoServers), [s for s in self.caseTwoServers if self.caseTwoServers[s]["Status"] == 0])
+
+    def testCaseThree(self):
+        self.assertEqual(findOptimalServer("A", self.caseThreeServers), None)
+
+    def testCaseFour(self):
+        self.assertIn(findOptimalServer("A", self.caseFourServers), [2,3])
+
+    def testCaseFive(self):
+        self.assertEqual(findOptimalServer("A", self.caseFiveServers), 3)
+
+    def testCaseSix(self):
+        self.assertEqual(findOptimalServer("A", self.caseSixServers), 2)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
 
 
